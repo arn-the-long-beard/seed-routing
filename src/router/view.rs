27e018693532ab6190b7fbfb2a093c24b8937482
arn-the-long-beard <arn.menant@gmail.com>
2020-10-30 15:33:40 +0100

    doc: added doc on view

--- src/router/view.rs
+++ src/router/view.rs
@@ -1,5 +1,7 @@
 use seed::prelude::Node;
 
+/// This trait allow you to display specific nodes in function of the current
+/// route and guards if needed
 pub trait View<Routes, State, Msg> {
     fn view(&self, scoped_state: &State) -> Node<Msg>;
 }
