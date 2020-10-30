b4f67fdb83157cdce65ba91aed5a3848d226cd5c
arn-the-long-beard <arn.menant@gmail.com>
2020-10-30 16:16:45 +0100

    doc: added more description regarding type for view and init

--- src/router/model.rs
+++ src/router/model.rs
@@ -2,6 +2,18 @@ use seed::prelude::Orders;
 
 /// The init on the route will call the init of their modules if they are not
 /// local view
-pub trait Init<Routes, State, Msg: 'static> {
-    fn init(&self, previous_state: &mut State, orders: &mut impl Orders<Msg>);
+///
+///
+/// # Routes
+/// The routes enum that when matching will give view a specific view   
+/// If the route contains payload ( query, id or children ) it will be passed to
+/// the init call so you can use it to init children routes or make state
+/// management
+/// # Model
+/// The standard model in TEA
+/// The model will be used by the init called and get updated
+/// # Msg
+/// The standard Msg Enum that we can use to triggers events or actions
+pub trait Init<Routes, Model, Msg: 'static> {
+    fn init(&self, previous_state: &mut Model, orders: &mut impl Orders<Msg>);
 }
--- src/router/view.rs
+++ src/router/view.rs
@@ -2,8 +2,23 @@ use seed::prelude::Node;
 
 /// This trait allow you to display specific nodes in function of the current
 /// route and guards if needed
-pub trait View<Routes, State, Msg> {
-    fn view(&self, scoped_state: &State) -> Node<Msg>;
+///
+/// # Routes
+/// The routes enum that when matching will give view a specific view   
+/// Nested Routes and children Routes are passed to the view to call view() on
+/// them as well
+/// # Model
+/// The model will be used by the view to display data in Html
+/// # Msg
+/// The standard Msg Enum that we can use to triggers events or actions
+///
+/// TODO :
+/// _______________________________________________________________________________
+/// Should we pass query and id parameter to view as well or having it in
+/// init is enough ?
+/// _______________________________________________________________________________
+pub trait View<Routes, Model, Msg> {
+    fn view(&self, scoped_state: &Model) -> Node<Msg>;
 }
 
 // pub trait Guarded<Routes, State, Msg> {
