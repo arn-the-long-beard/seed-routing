c0ed55135884c331164c9af5fa02852e8e22f853
arn-the-long-beard <arn.menant@gmail.com>
2020-10-30 15:40:08 +0100

    doc: added doc on init

--- src/router/model.rs
+++ src/router/model.rs
@@ -1,5 +1,7 @@
 use seed::prelude::Orders;
 
+/// The init on the route will call the init of their modules if they are not
+/// local view
 pub trait Init<Routes, State, Msg: 'static> {
     fn init(&self, previous_state: &mut State, orders: &mut impl Orders<Msg>);
 }
