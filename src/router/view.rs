fb23e6f0cd10f4bc6d2a75e8af7db0ccde94ff83
arn-the-long-beard <arn.menant@gmail.com>
2021-02-09 14:19:36 +0100

    doc: corrected typo error

--- src/router/view.rs
+++ src/router/view.rs
@@ -3,7 +3,7 @@ use seed::prelude::Node;
 /// This trait allows you to display specific nodes in function of the current
 /// route and guards if needed.
 /// View is not recursive. It does load the `local_view` matching `current_route()` or the `fn view` of the submodule.
-/// If the sub module contains nested or children routes, you need to call yourself their implementation of view to get the right view with `dashboard_routes.view(model)` as in the [example](https://github.com/arn-the-long-beard/seed-routing/blob/main/examples/backbone_app/src/pages/dashboard/mod.rs)
+/// If the sub module contains nested or children routes, you need to call yourself their implementation of view to get the right view with `dashboard_routes.view(model)` as in the [example](https://github.com/arn-the-long-beard/seed-routing/blob/main/examples/backbone_app/src/pages/dashboard/mod.rs).
 /// This rule is made to give maximum focus & control on the current module you are working with.
 /// # Routes
 /// The routes enum that when matching will give a specific view.
